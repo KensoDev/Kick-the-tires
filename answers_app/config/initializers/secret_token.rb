0d12937dcd334a89d72724dab83fd8005f18c045
Avi Tzurel <avi@kensodev.com>
2012-02-29 21:29:34 +0200

    Added a sample Rails 3.2 app to the exam

---
+++ answers_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnswersApp::Application.config.secret_token = 'a5b6d8210ddb470abb84f78e603b146c820d69e430e8abc702bf787ddc28947670521140153a8292feda169a4806f714a7e8dded01f6648070ec87c6a17c3f05'
